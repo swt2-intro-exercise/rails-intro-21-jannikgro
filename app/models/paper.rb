4409530149e8d5bc68710e8c4504fe1d661ef077
Jan Niklas Groeneveld <jan.niklas.groeneveld@gmail.com>
2021-11-20 17:38:59 +0100

    try #17 again

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,5 @@ class Paper < ApplicationRecord
     validates :title, presence: true
     validates :venue, presence: true
     validates :year, presence: true, numericality: { only_integer: true }
+    has_and_belongs_to_many :author
 end
