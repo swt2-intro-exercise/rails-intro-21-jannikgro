72fc557904677e6a7bf72e840f741dfd30821b4e
Jan Niklas Groeneveld <jan.niklas.groeneveld@gmail.com>
2021-11-20 16:32:18 +0100

    #12

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -2,6 +2,9 @@ class AuthorController < ApplicationController
     def new
         @author = Author.new
     end
+    def edit
+        @author = Author.find(params[:id])
+    end
     def create
         @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
 
@@ -11,7 +14,20 @@ class AuthorController < ApplicationController
             render "new"
         end
     end
+    def update
+        @author = Author.find(params[:id])
+       
+        if @author.update(author_params)
+          redirect_to @author
+        else
+          render 'edit'
+        end
+    end
     def show
         @author = Author.find(params[:id])
     end
+    private
+      def author_params
+        params.require(:author).permit(:first_name, :last_name, :homepage)
+      end
 end
